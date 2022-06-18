59febf328cc384ed83c543e0cd41952a7a1692d0
unknown <roger.hull@ucalgary.ca>
2022-06-18 12:52:33 -0700

    adding forum module

--- analysis_tool.py
+++ analysis_tool.py
@@ -4,60 +4,57 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-#alpaca_api_key = os.getenv("ALPACA_API_KEY")
-#alpaca_secret_key = os.getenv("ALPACA_SECRET_KEY")
+# Streamlit's layout is configured with contrainers (rows) and columns. The columns are nested withing the rows. The pages, such as our methodology page, must be held within a pages directory. Then, when we run the analysis tool using the "streamlit run analysis_tool.py" in our terminal, streamlit will look for the pages directory and add in any python files included.
 
-#api = tradeapi.REST(
-#    alpaca_api_key,
-#    alpaca_secret_key,
-#    api_version = "v2"
-#)
-
+# These are our containers.
 header = st.container()
 features = st.container()
 run = st.container()
 dataset = st.container()
 results = st.container()
 
-#with st.sidebar:
-#    st.sidebar.button('Analysis Tool')
-#    st.sidebar.button('Methodology')
+#Calling the header container.
 
 with header:
     st.title('Technical and Sentiment Stock Trading Algorithm')
     st.write('This web application is designed to help retail stock traders and investors determine if they should buy or sell a stock using news sentiment analysis and technical stock trading analysis. If you wish to learn more about our methodology, please visit the methodology tab')
     
+# Calling the features container
 with features:
-    st.subheader('Select Your Features')
-    col1, col2, col3, col4, col5, col6 = st.columns(6)
+    with st.form('Select Your Features'):
+        ticker = st.selectbox(
+            'Select your index',
+            ('NASDAQ','S&P 500','RUSSEL')
+        )
+        
+        date = st.selectbox(
+            'Date Range',
+            ('Dot Com Bubble',
+             '2008 Crash',
+             'Covid')
+        )
+        
+        nlp = st.checkbox('Use NYT Sentiment Analysis?')
+        
+        trading_options = ['MACD','SVM']
+        strategy = st.radio(
+            'Select your trading strategy',
+            trading_options
+        )
+        if strategy == 'MACD':
+            ema = st.slider('Days',0,60,5) #min: 0, max:60, def:5
+            sma = st.slider('Days',0,180,40) #min: 0, max:180, def:4
+        elif strategy == 'SVM':
+            pass
     
-    with col1:
-        date = st.selectbox('Date Range',('Dot Com Bubble','2008 Crash','Covid'))
-        st.write('You Selected:',date)
-        
-    with col2:
-        if st.checkbox('Use NYT Sentiment Analysis?'):
-            st.write('Sentiment Analysis has been added to the chart')
-        
-    with col3:
-        st.write("EMA Window")
-        st.slider('Days',0,60,5) #min: 0, max:60, def:5
-        
-    with col4:
-        st.write('SMA Window')
-        st.slider('Days',0,180,40) #min: 0, max:180, def:40
-        
-    with col5:
-        option = st.selectbox(
-        'Select your trading strategy',
-        ('MACD','SVM'))
-        
-    with col6:
-        stock = st.text_input('Select your stock','TSLA')
-        
-#with run:
-  #  if st.button('Run My Trading Algoritm'):
-        
+        submitted = st.form_submit_button('Run My Algorithm')
+        if submitted:
+            st.write(ticker, date, nlp, strategy)
+    
+    
+with run:
+    df = pd.read_csv(ticker)
+    
 
 with dataset:
     df = pd.read_csv('aapl.csv')
@@ -67,7 +64,9 @@ with dataset:
         height=0, 
         use_container_width=True
         )
-    
+
+#Calling the resutls container    
+
 with results:
     st.header('Trading Algorithm Results')
     col6, col7, col8, col9 = st.columns(4)
